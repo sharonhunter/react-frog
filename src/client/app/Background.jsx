7762c6b00183e7ba680e7a68c4eefe33d4d08150
sharonhunter <sharonhunter@runbox.com>
2016-10-09 20:59:21 -0400

    Refactor Frog and changeQuote interaction.

--- src/client/app/Background.jsx
+++ src/client/app/Background.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import Quote from './Quote.jsx';
 import QUOTES from './quotes.js';
+import Frog from './Frog.jsx';
 
 class Background extends React.Component {
 
   constructor(props) {
     super(props);
+    this.callChangeQuote = this.callChangeQuote.bind(this);
+  }
+
+  // wrapper function - the refs mechanism does not work until after first render
+  // this function will exist even before rendering
+  // Quote component requires ref = 'quote' for this to work!
+  callChangeQuote() {
+    this.refs.quote.changeQuote();
   }
 
   render() {
+
   	var style = {
         backgroundColor: "yellow",
         height: "100%",
@@ -16,7 +26,8 @@ class Background extends React.Component {
     }
     return (
       <div style={style}>
-        <Quote />
+        <Quote ref='quote' />
+        <Frog whenClicked={this.callChangeQuote} />
       </div>
     );
   }
